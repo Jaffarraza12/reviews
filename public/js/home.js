df6fdfc6b2d28019efb384bb103574ded34226b6
Jaffar Raza <jaffaraza@gmail.com>
2019-09-03 19:15:08 +0500

    Vue composer.json

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
+    delimiters: ['[[', ']]'],
     data: {
         reviews : null,
-        delimiters: ['[[', ']]'],
         fields: [
             {
                 key: 'name',

8196700911d6a6f0efcbb81a3b056e97b53c3f2d
jaffar raza <jaffaraza@gmail.com>
2019-09-04 15:51:30 +0500

    Vue composer.json

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,8 @@
         <div class="card">
             <div class="card-header">Recent Reviews</div>
             <div class="card-body">
+                [[reviews]]
+                [[items]]
                 <table class="table">
                     <tr>
                         <th>Name</th>

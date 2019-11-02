71425d08a75c6ed9cdc7fa2bbcc1fca53b526599
Jaffar Raza <jaffaraza@gmail.com>
2019-11-02 15:14:00 +0500

    Warranty Module.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Models\Contact;
+use App\Http\Models\Warranty;
 
 class ContactController extends Controller
 {
@@ -12,6 +13,12 @@ class ContactController extends Controller
     {
         $contact = Contact::create($request->all());
         return response()->json($contact, 201);
+    }
+
+    public function warranty(Request $request)
+    {
+        $warranty = Warranty::create($request->all());
+        return response()->json($warranty, 201);
     }
 
 }
---
+++ app/Http/Models/Warranty.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Warranty extends Model
+{
+    //
+    protected $table = 'warranty';
+    protected $connection = 'mysql';
+    protected $primaryKey = 'id';
+    protected $fillable = ['first_name','last_name','full_name','email','purchase_date','purchase_from','sell_price','comment'];
+}

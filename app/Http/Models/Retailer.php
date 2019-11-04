1a129c1f5233309d667170c4e7c8f4e896377ebf
Jaffar Raza <jaffaraza@gmail.com>
2019-11-04 18:41:05 +0500

    asddasdasdasdas

--- app/Http/Models/Retailer.php
+++ app/Http/Models/Retailer.php
@@ -10,5 +10,5 @@ class Retailer extends Model
     protected $table = 'retailer';
     protected $connection = 'mysql';
     protected $primaryKey = 'id';
-    protected $fillable = ['company_name','name','email','message','phone_number','sell'];
+    protected $fillable = ['company_name','name','email','message','website','phone_number','sell'];
 }

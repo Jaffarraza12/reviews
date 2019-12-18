93dd1069940fa709e111f6a969cd24a6392a8667
Jaffar Raza <jaffaraza@gmailcom>
2019-12-18 12:48:41 +0400

    home js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,9 +27,6 @@
         <div class="card">
             <div class="card-header">Recent Reviews</div>
             <div class="card-body">
-              [[review]]
-              [[items]]
-              [[data]]
                 <table class="table">
                     <tr>
                         <th>Name</th>

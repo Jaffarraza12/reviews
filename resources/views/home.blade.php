4f1afa6bb060131cba079817cb105caf6eedb018
Jaffar Raza <jaffaraza@gmailcom>
2019-12-23 14:36:03 +0400

    View Review

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,6 +34,7 @@
                         <th>Vote</th>
                         <th>Date </th>
                         <th>Publish </th>
+                        <th> </th>
 
                     </tr>
                     @foreach ($reviews as $rev)
@@ -42,7 +43,7 @@
                         <td>{{$rev->product}}</td>
                         <td>{{$rev->vote}} <i class="fa fa-star"></i></td>
                         <td>{{date('M d y',strtotime($rev->created_at))}}</td>
-                        <td><input type="checkbox"  data-toggle="toggle">  <a class="pointer" title="View Review"><i class="fa fa-search"></i></a></td>
+                        <td><input type="checkbox"  data-toggle="toggle">  <a class="pointer" title="View Review" data-toggle="modal" data-target="#viewReview"><i class="fa fa-search"></i></a></td>
                     </tr>
                     @endforeach
                 </table>

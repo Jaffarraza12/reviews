bf5e29b789905f0a97aaef179b09527fad1b18d4
jaffar raza <jaffaraza@gmail.com>
2020-01-01 00:19:24 +0500

    Phone Number

--- app/Http/Models/ProductRegister.php
+++ app/Http/Models/ProductRegister.php
@@ -10,6 +10,6 @@ class ProductRegister extends Model
     protected $table = 'product-register';
     protected $connection = 'mysql';
     protected $primaryKey = 'id';
-    protected $fillable = ['name','email','phone','country','state','city','zip','address','date','price','model','location'];
+    protected $fillable = ['name','email','phone_number','country','state','city','zip','address','date','price','model','location'];
 
 }
